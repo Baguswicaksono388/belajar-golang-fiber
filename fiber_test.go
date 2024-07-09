31afcea93373a9dab21503a25f4c6061db4ab71a
Bagus Wicaksono <baguswicaksono388@gmail.com>
2024-07-09 16:07:00 +0700

    upload first project

---
+++ fiber_test.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+var app = fiber.New()
+
+func TestRoutingHelloWorld(t *testing.T) {
+	app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello, World!")
+    })
+
+	request := httptest.NewRequest("GET", "/", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+	
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello, World!", string(bytes))
+}
+
+func TestCtx(t *testing.T) {
+	app.Get("/hello", func(ctx *fiber.Ctx) error {
+		name := ctx.Query("name", "Guest")
+        return ctx.SendString("Hello " + name)
+    })
+
+	request := httptest.NewRequest("GET", "/hello?name=Bagus", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+	
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Bagus", string(bytes))
+
+
+	request = httptest.NewRequest("GET", "/hello", nil)
+	response, err = app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+	
+	bytes, err = io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Guest", string(bytes))
+}
+
+
+func TestHttpRequest(t *testing.T) {
+	app.Get("/request", func(ctx *fiber.Ctx) error {
+		first := ctx.Get("firstname")
+		last := ctx.Cookies("lastname")
+        return ctx.SendString("Hello "+ first + " "+ last)
+    })
+
+	request := httptest.NewRequest("GET", "/request", nil)
+	request.Header.Set("firstname", "Bagus")
+	request.AddCookie(&http.Cookie{Name: "lastname", Value: "Wicaksono"})
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+	
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Bagus Wicaksono", string(bytes))
+}
+
+func TestRouteParamater(t *testing.T) {
+	app.Get("/users/:userId/orders/:orderId", func(ctx *fiber.Ctx) error {
+		userId := ctx.Params("userId")
+		orderId := ctx.Params("orderId")
+        return ctx.SendString("Get Order "+ orderId + " From User "+ userId	)
+    })
+
+	request := httptest.NewRequest("GET", "/users/Bagus/orders/10", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+	
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Get Order 10 From User Bagus", string(bytes))
+}
+
+func TestFormRequest(t *testing.T) {
+	app.Post("/hello", func(ctx *fiber.Ctx) error {
+		name := ctx.FormValue("name")
+        return ctx.SendString("Hello " + name)
+    })
+
+	body := strings.NewReader("name=Bagus")
+	request := httptest.NewRequest("POST", "/hello", body)
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+	
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Bagus", string(bytes))
+}
+
+
+//go:embed source/contoh.txt
+var contohFile []byte
+func TestFormUpload(t *testing.T) {
+	app.Post("/upload", func(ctx *fiber.Ctx) error {
+		file, err := ctx.FormFile("file")
+		
+		if err!= nil {
+            return err
+        }
+		
+		err = ctx.SaveFile(file, "./target/"+file.Filename)
+		if err!= nil {
+			return err
+		}
+
+		return ctx.SendString("Upload Success")
+    })
+
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	file, err := writer.CreateFormFile("file", "contoh.txt")
+	assert.Nil(t, err)
+	file.Write(contohFile)
+	writer.Close()
+
+	request := httptest.NewRequest("POST", "/upload", body)
+	request.Header.Set("Content-Type", writer.FormDataContentType())
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+	
+    bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Upload Success", string(bytes))
+}
