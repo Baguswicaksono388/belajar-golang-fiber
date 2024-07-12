d6370182be73d1953e8f1d2825d998c0d54f2707
Bagus Wicaksono <baguswicaksono388@gmail.com>
2024-07-12 11:07:07 +0700

    Prefork

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,7 +12,15 @@ func main() {
 		IdleTimeout: time.Second * 5,
 		WriteTimeout: time.Second * 5,
 		ReadTimeout: time.Second * 5,
+		// Prefork: true,
 	})
+
+
+	// if fiber.IsChild() {
+	// 	fmt.Println("I'm a child")
+	// } else {
+	// 	fmt.Println("I'm a parent")
+	// }
 
 	app.Get("/", func(c *fiber.Ctx) error {
         return c.SendString("Hello, World 👋!")
